4b39067c9f44ff5b55abe272b2218c7a3ee8cfa6
PetrKavan31 <petrkavan31@gmail.com>
2024-09-26 11:07:48 +0200

    find solution

--- main.py
+++ main.py
@@ -1,2 +1,38 @@
+# Complete the solution so that it splits the string into pairs of two characters.
+# If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+#
+# Examples:
+#
+# * 'abc' =>  ['ab', 'c_']
+# * 'abcdef' => ['ab', 'cd', 'ef']
+
+
 def solution(s):
-    pass
+
+    t = []
+    r = []
+    u = []
+
+    for i in range(0, len(s), 2):
+        t.append(s[i])
+
+    if len(s) % 2 != 0:
+        for i in range(1, len(s), 2):
+            r.append(s[i])
+        r.append("_")
+    else:
+        for i in range(1, len(s), 2):
+            r.append(s[i])
+
+    for i in range(len(t)):
+        u.append(t[i] + r[i])
+    return u
+
+
+print(solution('abcd'))
+print(solution('abc'))
+print(solution('a'))
+print(solution(""))
+
+
+
--- test_code.py
+++ test_code.py
@@ -1,17 +1,27 @@
-s = 'abcd'
+s = list('abcd')
+print(s)
 t = []
 r = []
 u = []
+#w = []
 #if len(s) % 2 != 0:
-    #s.append('_')
+    #for i in range(len(s)):
+        #w.append(s[i])
+    #w.append("_")
+    #print(w)
 for i in range(0, len(s), 2):
 #for i in range(len(s)):
     t.append(s[i])
 
     #t = t + s[i]
 print(t)
-for i in range(1, len(s), 2):
-    r.append(s[i])
+if len(s) % 2 != 0:
+    for i in range(1, len(s), 2):
+        r.append(s[i])
+    r.append("_")
+else:
+    for i in range(1, len(s), 2):
+        r.append(s[i])
 
 print(r)
 for i in range(len(t)):

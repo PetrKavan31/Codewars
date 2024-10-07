2e4100c3fc0e29dc09eba2b590868884f0f1795c
PetrKavan31 <petrkavan31@gmail.com>
2024-10-07 20:55:38 +0200

    edit Counter() - one line

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 from collections import Counter
 
 
-def count_occurrence(a):
-    return Counter(a)
-
-
 def key_with_odd_value():
-    return next(d for d, i in count_occurrence(b).items() if i % 2 != 0)
+    return next(d for d, i in Counter(b).items() if i % 2 != 0)
 
 
 b = [12, 8, 8, 42, 12]

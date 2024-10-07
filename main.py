c0eff13e5a54f8113b07cdcbb1fb6a864845f53f
PetrKavan31 <petrkavan31@gmail.com>
2024-10-07 17:58:55 +0200

    step 1 and 2

--- main.py
+++ main.py
@@ -1,29 +1,17 @@
-# Two sum
+def count_occurrence(a):
+    d = {}
+    for i in a:
+        x = a.count(i)
+        d.update({i: x})
 
-# Write a function that takes an array of numbers (integers for the tests) and a target number.
-# It should find two different items in the array that, when added together, give the target value.
-# The indices of these items should then be returned in a tuple / list (depending on your language) like so: (index1, index2).
-#
-# For the purposes of this kata, some tests may have multiple answers; any valid solutions will be accepted.
-#
-# The input will always be valid (numbers will be an array of length 2 or greater, and all of the items will be numbers;
-# target will always be the sum of two different items from that array).
-#
-# Based on: https://leetcode.com/problems/two-sum/
-
-import itertools
+    return d
 
 
-def two_sum(numbers, target):
-    return next((i, j) for i, j in itertools.combinations(range(len(numbers)), 2) if numbers[i] + numbers[j] == target)
+def key_with_odd_value():
+    for key, val in count_occurrence(b).items():
+        if val % 2 != 0:
+            return key
 
 
-print(two_sum([1, 2, 3], 4))
-assert two_sum([1, 2, 3], 4) in ((0, 2), (2, 0))
-print(two_sum([3, 2, 4], 6))
-assert two_sum([3, 2, 4], 6) in ((1, 2), (2, 1))
-print(two_sum([2, 1, 3, 8], 4))
-assert two_sum([2, 1, 3, 8], 4) in ((1, 2), (2, 1))
-
-
-
+b = [12, 8, 8, 42, 12]
+print(key_with_odd_value())
--- test_code.py
+++ test_code.py
@@ -1,55 +1,22 @@
-# a = [1, 1, 2]
-#
-# for i in range(len(a)-1):
-#     count = 0
-#     for j in range(1, len(a)):
-#         if a[i] == a[j]:
-#             if j >= i:
-#                 count += 1
-#
-#     print(count)
+
+def count_occurrence(a):
+    d = {}
+    for i in a:
+        x = a.count(i)
+        d.update({i: x})
+
+    return d
 
 
-# a = [0, 1, 0, 1, 0]
-# count = 0
-# for i in range(len(a)-1):
-#     for j in range(1, len(a)):
-#         if a[i] == a[j]:
-#             if i != j:
-#                 count += 1
-# print(count)
+# b = [12, 8, 8, 42, 12]
+# print(count_occurrence(b))
 
 
-# a = [1, 2, 2, 2, 1]
-#
-# for i in range(len(a)):
-#     count = 0
-#     for j in range(len(a)):
-#         if a[i] == a[j]:
-#             if j != i:
-#                 count += 1
-#     if count % 2 == 0:
-#         break
-# print(a[i])
+def key_with_odd_value():
+    for key, val in count_occurrence(b).items():
+        if val % 2 != 0:
+            return key
 
-# two_sum([1, 2, 3], 4)
-# numbers = [1, 2, 3]
-# target = 4
-# a = 0
-# for i in range(len(numbers)):
-#     a = target - numbers[i]
-#     for j in range(len(numbers)):
-#         if j == a:
-#             print(j)
 
-numbers = [3, 2, 4]
-target = 6
-a = 0
-for i in range(len(numbers)):
-    a = target - numbers[i]
-    for j in range(len(numbers)):
-        if numbers[j] == a:
-            #print(numbers[j])
-            break
-
-print((i,j))
+b = [12, 8, 8, 42, 12]
+print(key_with_odd_value())

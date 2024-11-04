a8bf8b65570215e9078ac2043763b9633ac574e0
PetrKavan31 <petrkavan31@gmail.com>
2024-11-04 22:30:35 +0100

    Convert string to camel case

--- test_code.py
+++ test_code.py
@@ -1,23 +1,7 @@
-def solution(c):
-
-    a = ((1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'),
-         (100, 'C'), (90, 'XC'), (50, 'L'), (40, 'XL'),
-         (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I'))
-    aa = []
-    for i in a:
-        if c // i[0] == 1:
-            c = c - i[0]
-            aa.append((i[1]))
-        if c // i[0] == 2:
-            c = c - (i[0]*2)
-            aa.append(2*(i[1]))
-        if c // i[0] == 3:
-            c = c - (i[0]*3)
-            aa.append(3*(i[1]))
-    return "".join(aa)
+a = 'the-Stealth-Warrior'
+b = 'The_Stealth_Warrior'
+c = list('The_Stealth-Warrior')
+print(c)
 
 
-assert solution(1889) == 'MDCCCLXXXIX'
-assert solution(1) == 'I'
-assert solution(984) == 'CMLXXXIV'
-assert solution(21) == 'XXI'
+

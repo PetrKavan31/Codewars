b301954e31842eedcb430ade9ea177587dc28b6e
PetrKavan31 <petrkavan31@gmail.com>
2024-11-04 16:16:41 +0100

    test code

--- Find_the_odd_int.py
+++ Find_the_odd_int.py
@@ -52,3 +52,34 @@ def find_it(seq):
     for x in seq:
         result ^= x
     return result
+
+
+# ---------------------------- my test ---------------------------------------------------
+
+
+def count_occurrence(a):
+    d = {}
+    for i in a:
+        if i in d:
+            d[i] += 1
+        else:
+            d[i] = 1
+
+    #     x = a.count(i)
+    #     d.update({i: x})
+    #
+    return d
+
+
+# b = [12, 8, 8, 42, 42, 42, 12]
+# print(count_occurrence(b))
+
+
+def key_with_odd_value():
+    for key, val in count_occurrence(b).items():
+        if val % 2 != 0:
+            return key
+
+
+b = [12, 8, 8, 42, 42, 42, 12]
+print(key_with_odd_value())

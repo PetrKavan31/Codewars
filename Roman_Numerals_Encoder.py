a34483899c22a792f1d4cb1373ad620474b621a0
PetrKavan31 <petrkavan31@gmail.com>
2024-11-04 20:19:13 +0100

    add task text

--- Roman_Numerals_Encoder.py
+++ Roman_Numerals_Encoder.py
@@ -1,3 +1,14 @@
+# Create a function taking a positive integer between 1 and 3999 (both included) as its parameter and returning a string containing the Roman Numeral representation of that integer.
+#
+# Modern Roman numerals are written by expressing each digit separately starting with the leftmost digit and skipping any digit with a value of zero.
+# There cannot be more than 3 identical symbols in a row.
+#
+# In Roman numerals:
+#
+# 1990 is rendered: 1000=M + 900=CM + 90=XC; resulting in MCMXC.
+# 2008 is written as 2000=MM, 8=VIII; or MMVIII.
+# 1666 uses each Roman symbol in descending order: MDCLXVI.
+
 def solution(c):
 
     a = ((1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'),
